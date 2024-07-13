2f7501f7988621b130293963533c200e1194f669
Andrew Maurice <andrewmaurice123@gmail.com>
2024-07-13 12:48:24 +0300

    adding balancing

--- src/AvlTree.java
+++ src/AvlTree.java
@@ -42,14 +42,58 @@ public class AvlTree {
         // setting the node height.
         node.height = height(node);
 
-        if(isLeftHeavy(node)) {
-            System.out.println(node.value + " is left heavy a right rotation is required");
-        } 
-        else if (isRightHeavy(node)) {
-            System.out.println(node.value + " is right heavy, a left rotation is required");
-        }
+        root = balance(node);
 
         return node;
+    }
+
+    private AvlTreeNode balance(AvlTreeNode root) {
+        
+        if(isLeftHeavy(root)) {
+            var rootNodeLeftChildBalanceFactor = balanceFactor(root);
+
+            // if the balance factor of the tree is less than 0 then we need to perform a right rotation.
+            if(rootNodeLeftChildBalanceFactor < 0) {
+                root.left = rotateLeft(root.left);
+            }
+
+            // Since the tree is already left heavy then we need to perform left rotation.
+            root = rotateRight(root);
+
+        } 
+        else if (isRightHeavy(root)) {
+            var rootNodeRightChildBalanceFactor = balanceFactor(root.right);
+
+            // if the balance factor of the right child is > 0 then we need to perform a left rotation
+            if(rootNodeRightChildBalanceFactor > 0) {
+                root.right = rotateRight(root.right);
+            }
+            // since the tree is right heavy then we must perform a left rotate.
+            root = rotateLeft(root);
+        }
+
+        return root;
+    }
+
+    private AvlTreeNode rotateLeft(AvlTreeNode root) {
+        AvlTreeNode newRoot = root.right;
+        root.right = newRoot.left;
+        newRoot.left = root;
+        newRoot.height = height(newRoot);
+        root.height = height(root);
+
+        return newRoot;
+    }
+
+    private AvlTreeNode rotateRight(AvlTreeNode root) {
+        AvlTreeNode newRoot = root.left;
+        root.left = newRoot.right;
+        newRoot.right = root;
+
+        newRoot.height = height(newRoot);
+        root.height = height(root);
+
+        return newRoot;
     }
 
     public int height() {
